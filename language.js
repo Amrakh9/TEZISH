2b8fe267545e653121628367eb80568e19bb4f71
amrakh9 <emrahamiraslanov@gmail.com>
2022-11-16 11:28:50 +0400

    some link for language

--- language.js
+++ language.js
@@ -1,1 +1,9 @@
+// https://www.geeksforgeeks.org/currency-converter-in-javascript/
+
+
 // https://benfrain.com/creating-a-language-switcher-in-javascript/
+
+// https://stackoverflow.com/questions/70783026/how-to-switch-language-by-toggle-button
+
+// https://www.geeksforgeeks.org/how-to-switch-the-language-of-the-page-using-javascript/?tab=article
+// https://weglot.com/9-tips-for-designing-a-multi-language-website/
